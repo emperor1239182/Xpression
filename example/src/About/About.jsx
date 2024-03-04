d5cee834aca5eead3a375fd2ef2bb3991d6717c7
Xpression <emmybanks03@gmail.com>
2024-03-04 01:59:06 +0100

    RWD

--- example/src/About/About.jsx
+++ example/src/About/About.jsx
@@ -6,20 +6,26 @@ const About = () => {
     const about = {
         display: 'block',
         margin: '10px',
-        fontSize: '1rem'
+        fontSize: '1rem',
+        fontFamily: 'Cambria, Cochin, Georgia, Times, serif'
     }
 
 
     return (
         <>
-        <div style={about}>
-            <h2 style={{textAlign:'center', margin:'10px'}}>About Us</h2>
+        <div style={about} className='row'>
+        <h2 style={{textAlign:'center', margin:'10px'}}>About Us</h2>
+            <div className='col-9'>
+            
             <p>At Xpression we bring to you the home of quality, luxurious and affordable clothing. <br/>
             Our products aren't limited to the Luxury lovers alone.. Are you a fashionista? Xpression is the best place for you to get any kind of fashion clothing of your choice. <br />
             We aren't just about your clothes alone, we are also concerned about the most fancy footwear to give you a complete outstanding outfit <br />
             We've got you outfits for all seasons, occassion, outing... wanna go on a date with your loved ones? we got you covered <br />
             </p>
+            </div>
+            <div className='col-3'>
             <img src="/public/swag.JPG" style={{width:'300px', height:'400px', marginTop:'10px'}}/>
+            </div>
             <p>With over 20,000 delivery nationwide, we are across over 7 countries nationwide (South Africa, Nigeria, United States, Japan, France, Italy, Russia )</p>
         </div>
         <section className="footer">
